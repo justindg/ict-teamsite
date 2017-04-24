c66f891e6d2c9e7261b878853c3fa2e3d96404a5
justindg <jrbdeguzman@gmail.com>
2017-04-24 15:30:28 +1000

    added icons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 $('.service').on('mouseenter', function() {
   $(this).find('.service_container').hide();
-  $(this).find('.service_container_back').fadeIn();
+  $(this).find('.service_container_back').show();
+  $(this).find('.service_container_back').find('.service_back_item').slideDown(300);
 });
 
 $('.service').on('mouseleave', function() {
   $(this).find('.service_container').show();
   $(this).find('.service_container_back').hide();
+  $(this).find('.service_container_back').find('.service_back_item').slideUp(300);
+});
 
+$('.service_ui').on('click', function() {
+  window.location.href = 'innovation.html';
+});
+
+$('.service_ic').on('click', function() {
+  window.location.href = 'coaching.html';
+});
+
+$('.service_rp').on('click', function() {
+  window.location.href = 'prototyping.html';
 });
