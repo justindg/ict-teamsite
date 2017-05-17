db8869979a34f40d1ebf74b1c8361f3ac04b0c08
justindg <jrbdeguzman@gmail.com>
2017-05-18 01:15:26 +1000

    revised version

--- js/main.js
+++ js/main.js
@@ -15,6 +15,36 @@ $('.interlock_carousel').slick({
   pauseOnHover: false
 });
 
+var closeStartHelper = function() {
+  if ($('.helper_start').is(':visible')) {
+    $('.helper_start').fadeOut(100);
+  }
+}
+
+$('.pillar_ic').on('click', function() {
+  closeStartHelper();
+  $('.pillar_container').removeClass('pillar_container_active');
+  $(this).addClass('pillar_container_active');
+  $('.content').slideUp(300);
+  $('.content_ic').slideDown(500);
+});
+
+$('.pillar_ui').on('click', function() {
+  closeStartHelper();
+  $('.pillar_container').removeClass('pillar_container_active');
+  $(this).addClass('pillar_container_active');
+  $('.content').slideUp(300);
+  $('.content_ui').slideDown(500);
+});
+
+$('.pillar_rp').on('click', function() {
+  closeStartHelper();
+  $('.pillar_container').removeClass('pillar_container_active');
+  $(this).addClass('pillar_container_active');
+  $('.content').slideUp(300);
+  $('.content_rp').slideDown(500);
+});
+
 $('.service').on('mouseenter', function() {
   $(this).find('.service_container').hide();
   $(this).find('.service_container_back').show();
